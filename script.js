9e73e67f7fc87442cdb784778a3c9edcb9f0383c
Harsh Arya <harsharya8946@gmail.com>
2026-03-02 23:46:42 +0530

    like feature is completed

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 let abtn = document.querySelector('.text button');
+let limg = document.querySelector('.postcard img');
+let emote = document.querySelector('.postcard i'); 
 
-let like = false;
+let add = false;
 abtn.addEventListener('click',function(){
-    like = !like;
-    if(like){
+    add = !add;
+    if(add){
         abtn.style.backgroundColor = 'white';
         abtn.style.color = 'rgb(89, 89, 255)';
         abtn.style.border = '2px solid rgb(89, 89, 255)';
@@ -13,6 +15,28 @@ abtn.addEventListener('click',function(){
         abtn.style.backgroundColor = 'rgb(89, 89, 255)';
         abtn.style.color = 'white';
         abtn.innerHTML = 'Follow';
-    }
+    } 
+})
+
+let count = 0;
+limg.addEventListener('dblclick',function(){
+    console.log('click');
+    emote.style.opacity = 1;
+    setTimeout(()=>{
+       count--;
+       emote.style.top = count+'px';
+       console.log(count); 
+    },500)
+    setTimeout(function(){
+      emote.style.opacity = 0;  
+    },1300)
     
+    setTimeout(() => {
+
+        emote.style.transition = "none";
+        emote.style.top = "100px";
+        emote.offsetHeight;
+        emote.style.transition = "all 2s ease-in-out";
+
+    }, 2000);
 })
